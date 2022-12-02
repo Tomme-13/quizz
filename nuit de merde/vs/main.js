032d126c9e38339adc857f07d5fb8ace306221d8
Tomme-13 <tomwussler@gmail.com>
2022-12-02 01:00:27 +0100

    main

--- nuit de merde/vs/main.js
+++ nuit de merde/vs/main.js
@@ -1,41 +1,35 @@
 const Questions = [{
     id: 0,
     q: "Qu'est-ce qu'une IST?",
-    a: [{ text: "", isCorrect: false },
-        { text: "", isCorrect: false },
     a: [{ text: "Institut du Sex Toy", isCorrect: false },
         { text: "Initiation à la Sexualité Tantrique", isCorrect: false },
         { text: "Infection Sexuellement Transmissible", isCorrect: true },
-        { text: "", isCorrect: false }
-        { text: "Interdit de sextoter au travail", isCorrect: false }
+        { text: "", isCorrect: false },
+        { text: "Interdit de sextoter au travail", isCorrect: false },
     ]
 
 },
 {
     id: 1,
-    q: "QUEL EST LE MOYEN DE CONTRACEPTION LE PLUS EFFICACE ?",
     q: "Quel est le moyen de contraception le plus efficace ?",
     a: [{ text: "la pilule contraceptive", isCorrect: false, isSelected: false },
         { text: "porter des chaussettes au lit", isCorrect: false },
         { text: "le diaphragme", isCorrect: false },
-@ -22,12 +22,44 @@ const Questions = [{
+]
+},
 {
     id: 2,
     q: "Combien de temps dure en moyenne un rapport sexuel ?",
-    a: [{ text: "", isCorrect: false },
-        { text: "", isCorrect: false },
-        { text: "", isCorrect: true },
-        { text: "", isCorrect: false }
     a: [{ text: "12 minutes", isCorrect: false },
         { text: "25 secondes", isCorrect: false },
         { text: "5,4 minutes", isCorrect: true },
         { text: "ça depend des piles", isCorrect: false }
     ]
 
-}
+},
 {
     id: 3,
-    q: "L'ejaculation precoce est un problème qui touche..."
+    q: "L'ejaculation precoce est un problème qui touche...",
     a: [{ text: "moins de 10% des hommes", isCorrect: false},
         { text: "entre 50% et 60% des hommes", isCorrect: true},
         {text: "seulement les mecs bien car ce sont eux qui partent les premiers", isCorrect: false}
@@ -44,7 +38,7 @@ const Questions = [{
 },
 {
     id: 4,
-    q: "Outre le VIH/SIDA, quelle est l'IST que l'on garde à vie"
+    q: "Outre le VIH/SIDA, quelle est l'IST que l'on garde à vie",
     a: [{ text: "La chlamydiae", isCorrect: false},
         { text: "L'herpès génital", isCorrect: true},
         {text: "La syphillis", isCorrect: false},
@@ -52,11 +46,11 @@ const Questions = [{
  
  ]    
 
-}
+},
 
 {
     id: 5,
-    q: "Comment peut on attraper le VIH ou le SIDA"
+    q: "Comment peut on attraper le VIH ou le SIDA",
     a: [{ text: "Par piqûre de moustique", isCorrect: false},
         { text: "Lors de rapports sexuels non protégés par un préservatif", isCorrect: true},
         {text: "En buvant dans le verre d'une personne séropositive", isCorrect: false},
